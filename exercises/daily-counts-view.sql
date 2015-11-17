16e4abadb1fddcb372b3c934b7c74f3bd56ea91b
Marco Slot <marco@citusdata.com>
2015-11-17 15:47:54 +0000

    Rename view column to value

--- exercises/daily-counts-view.sql
+++ exercises/daily-counts-view.sql
@@ -1,5 +1,5 @@
 CREATE OR REPLACE VIEW data_daily_counts AS
-SELECT created_date, SUM(value)
+SELECT created_date, SUM(value) AS value
 FROM (
     SELECT created_date, value
     FROM data_daily_counts_cached
